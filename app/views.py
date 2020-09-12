b31ef73a6834ec85794f2c7ccfdbffcf0c41df45
legionpunisher <mbuguamoses656@gmail.com>
2020-09-12 21:28:15 +0300

    made an api call to get the news category

--- app/views.py
+++ app/views.py
@@ -1,13 +1,18 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 #Views
-@app.route ('/')
-def index():
-    
-    title = 'Homep welcome to the best news website online'
-    return render_template('index.html',title = title)
 
-@app.route ('/news/<int:news_id>')
-def news(news_id):
-    return render_template('news.html',id = news_id)
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting news category
+    news_category = get_news('category')
+    print(news_category)
+    title = 'THE BEST NEWS SITE IN THE WORLD'
+    return render_template('index.html', title = title,category = news_category)
