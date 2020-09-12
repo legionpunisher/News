9aa26203a38f82c3cca6c11b0bdc18f7422b832a
legionpunisher <mbuguamoses656@gmail.com>
2020-09-12 21:21:29 +0300

    created our first api call processed the news source get from our Api key

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ class Config:
     '''
     pass
     # NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}'
-    NEWS_API_BASE_URL = 'https://newsapi.org/v1/articles?source=the-next-web&apiKey={}'
-
+    # NEWS_API_BASE_URL = 'https://newsapi.org/v1/articles?source=the-next-web&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
 
 
 class ProdConfig(Config):
