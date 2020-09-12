5b4ec0d93056486b084a958000c94a6e8ea9b3ff
legionpunisher <mbuguamoses656@gmail.com>
2020-09-12 14:25:22 +0300

    created configuration classes that will be used in either production and development stages or both

--- app/views.py
+++ app/views.py
@@ -4,4 +4,10 @@ from app import app
 #Views
 @app.route ('/')
 def index():
-    return render_template('index.html')
+    
+    message = 'a new news'
+    return render_template('index.html',message = message)
+
+@app.route ('/news/<int:news_id>')
+def news(news_id):
+    return render_template('news.html',id = news_id)
