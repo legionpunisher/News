da63f233852e7e6285e9dd5cbe7ae9c883c5a6af
legionpunisher <mbuguamoses656@gmail.com>
2020-09-12 14:29:38 +0300

    added our configuration to the init.py so that our application use them

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from .config import DevConfig
 # we initialize our applicarion
 app = Flask(__name__)
+
+#we set up configuration
+app.config.from_object(DevConfig)
 
 from app import views
