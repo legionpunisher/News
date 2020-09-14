62bdaa00015dc24bf37c01970852cff937195e5d
legionpunisher <mbuguamoses656@gmail.com>
2020-09-14 22:14:30 +0300

    added our views to main folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,12 +1,3 @@
-from flask_bootstrap import Bootstrap
-from flask import Flask
-from .config import DevConfig
-# we initialize our applicarion
-app = Flask(__name__,instance_relative_config= True)
-
-# set up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
-
-bootstrap = Bootstrap(app)
-from app import views
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from .import views,error
--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,20 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_source,article_source,get_category,get_headlines
 from newsapi import NewsApiClient
-from .request import get_sources,get_source
+
+
 
 #Views
-
 @main.route('/')
 def index():
-
     '''
-    View root page function that returns the index page and its data
+    Root function returning index/home page with data
     '''
+    source= get_source()
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
 
-     # Getting popular movie
-    source = get_sources()       
-    # print(source)
-    title = 'THE BEST NEWS SITE IN THE WORLD'
-    return render_template('index.html', title = title,sources=source)
 @main.route('/article/<id>')
 def article(id):
 
@@ -26,3 +24,14 @@ def article(id):
     # title= 'Articles'
     articles = article_source(id)
     return render_template('article.html',articles= articles,id=id )
+
+@main.route('/categories/<cat_name>')
+def category(cat_name):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+    cat = cat_name
+
+    return render_template('categories.html',title = title,category = category, cat= cat_name)
